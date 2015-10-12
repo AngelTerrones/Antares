af7a7da8702110b9c34d9aa70315c9caa7180f4f
Angel Terrones <angelterrones@gmail.com>
2015-10-11 23:50:08 -0430

    Bug Fix: Ignore stall if flush is asserted.
    Avoid an error when a mem instruction fails, and the next instruction
    have a data dependency (stalls for mem).

--- Hardware/antares_idex_register.v
+++ Hardware/antares_idex_register.v
@@ -1,7 +1,7 @@
 //==================================================================================================
 //  Filename      : antares_idex_register.v
 //  Created On    : Sat Sep  5 21:08:59 2015
-//  Last Modified : Sun Sep 06 11:18:17 2015
+//  Last Modified : Sun Oct 11 23:40:16 2015
 //  Revision      : 1.0
 //  Author        : Angel Terrones
 //  Company       : Universidad Simón Bolívar
@@ -98,32 +98,32 @@ module antares_idex_register (/*AUTOARG*/
     // Clear only critical signals: op, WE, MEM write and Next PC
     //--------------------------------------------------------------------------
     always @(posedge clk) begin
-        ex_alu_operation        <= (rst) ? 5'b0  : ((ex_stall) ? ex_alu_operation        : ((id_stall | id_flush) ? 5'b0 : id_alu_operation));
-        ex_data_rs              <= (rst) ? 32'b0 : ((ex_stall) ? ex_data_rs                                              : id_data_rs);
-        ex_data_rt              <= (rst) ? 32'b0 : ((ex_stall) ? ex_data_rt                                              : id_data_rt);
-        ex_gpr_we               <= (rst) ? 1'b0  : ((ex_stall) ? ex_gpr_we               : ((id_stall | id_flush) ? 1'b0 : id_gpr_we));
-        ex_mem_to_gpr_select    <= (rst) ? 1'b0  : ((ex_stall) ? ex_mem_to_gpr_select    : ((id_stall | id_flush) ? 1'b0 : id_mem_to_gpr_select));
-        ex_mem_write            <= (rst) ? 1'b0  : ((ex_stall) ? ex_mem_write            : ((id_stall | id_flush) ? 1'b0 : id_mem_write));
-        ex_alu_port_a_select    <= (rst) ? 2'b0  : ((ex_stall) ? ex_alu_port_a_select                                    : id_alu_port_a_select);
-        ex_alu_port_b_select    <= (rst) ? 2'b0  : ((ex_stall) ? ex_alu_port_b_select                                    : id_alu_port_b_select);
-        ex_gpr_wa_select        <= (rst) ? 2'b0  : ((ex_stall) ? ex_gpr_wa_select                                        : id_gpr_wa_select);
-        ex_mem_byte             <= (rst) ? 1'b0  : ((ex_stall) ? ex_mem_byte                                             : id_mem_byte);
-        ex_mem_halfword         <= (rst) ? 1'b0  : ((ex_stall) ? ex_mem_halfword                                         : id_mem_halfword);
-        ex_mem_data_sign_ext    <= (rst) ? 1'b0  : ((ex_stall) ? ex_mem_data_sign_ext                                    : id_mem_data_sign_ext);
-        ex_rs                   <= (rst) ? 5'b0  : ((ex_stall) ? ex_rs                                                   : id_rs);
-        ex_rt                   <= (rst) ? 5'b0  : ((ex_stall) ? ex_rt                                                   : id_rt);
-        ex_dp_hazard            <= (rst) ? 4'b0  : ((ex_stall) ? ex_dp_hazard            : ((id_stall | id_flush) ? 4'b0 : id_dp_hazard));
-        ex_sign_imm16           <= (rst) ? 17'b0 : ((ex_stall) ? ex_sign_imm16                                           : id_imm_extended);
-        ex_cp0_data             <= (rst) ? 32'b0 : ((ex_stall) ? ex_cp0_data                                             : id_cp0_data);
-        ex_exception_pc         <= (rst) ? 32'b0 : ((ex_stall) ? ex_exception_pc                                         : id_exception_pc);
-        ex_movn                 <= (rst) ? 1'b0  : ((ex_stall) ? ex_movn                 : ((id_stall | id_flush) ? 1'b0 : id_movn));
-        ex_movz                 <= (rst) ? 1'b0  : ((ex_stall) ? ex_movz                 : ((id_stall | id_flush) ? 1'b0 : id_movz));
-        ex_llsc                 <= (rst) ? 1'b0  : ((ex_stall) ? ex_llsc                                                 : id_llsc);
-        ex_kernel_mode          <= (rst) ? 1'b0  : ((ex_stall) ? ex_kernel_mode                                          : id_kernel_mode);
-        ex_is_bds               <= (rst) ? 1'b0  : ((ex_stall) ? ex_is_bds                                               : id_is_bds);
-        ex_trap                 <= (rst) ? 1'b0  : ((ex_stall) ? ex_trap                 : ((id_stall | id_flush) ? 1'b0 : id_trap));
-        ex_trap_condition       <= (rst) ? 1'b0  : ((ex_stall) ? ex_trap_condition                                       : id_trap_condition);
-        ex_ex_exception_source  <= (rst) ? 1'b0  : ((ex_stall) ? ex_ex_exception_source  : ((id_stall | id_flush) ? 1'b0 : id_ex_exception_source));
-        ex_mem_exception_source <= (rst) ? 1'b0  : ((ex_stall) ? ex_mem_exception_source : ((id_stall | id_flush) ? 1'b0 : id_mem_exception_source));
+        ex_alu_operation        <= (rst) ? 5'b0  : ((ex_stall & ~id_flush) ? ex_alu_operation        : ((id_stall | id_flush) ? 5'b0 : id_alu_operation));
+        ex_data_rs              <= (rst) ? 32'b0 : ((ex_stall & ~id_flush) ? ex_data_rs                                              : id_data_rs);
+        ex_data_rt              <= (rst) ? 32'b0 : ((ex_stall & ~id_flush) ? ex_data_rt                                              : id_data_rt);
+        ex_gpr_we               <= (rst) ? 1'b0  : ((ex_stall & ~id_flush) ? ex_gpr_we               : ((id_stall | id_flush) ? 1'b0 : id_gpr_we));
+        ex_mem_to_gpr_select    <= (rst) ? 1'b0  : ((ex_stall & ~id_flush) ? ex_mem_to_gpr_select    : ((id_stall | id_flush) ? 1'b0 : id_mem_to_gpr_select));
+        ex_mem_write            <= (rst) ? 1'b0  : ((ex_stall & ~id_flush) ? ex_mem_write            : ((id_stall | id_flush) ? 1'b0 : id_mem_write));
+        ex_alu_port_a_select    <= (rst) ? 2'b0  : ((ex_stall & ~id_flush) ? ex_alu_port_a_select                                    : id_alu_port_a_select);
+        ex_alu_port_b_select    <= (rst) ? 2'b0  : ((ex_stall & ~id_flush) ? ex_alu_port_b_select                                    : id_alu_port_b_select);
+        ex_gpr_wa_select        <= (rst) ? 2'b0  : ((ex_stall & ~id_flush) ? ex_gpr_wa_select                                        : id_gpr_wa_select);
+        ex_mem_byte             <= (rst) ? 1'b0  : ((ex_stall & ~id_flush) ? ex_mem_byte                                             : id_mem_byte);
+        ex_mem_halfword         <= (rst) ? 1'b0  : ((ex_stall & ~id_flush) ? ex_mem_halfword                                         : id_mem_halfword);
+        ex_mem_data_sign_ext    <= (rst) ? 1'b0  : ((ex_stall & ~id_flush) ? ex_mem_data_sign_ext                                    : id_mem_data_sign_ext);
+        ex_rs                   <= (rst) ? 5'b0  : ((ex_stall & ~id_flush) ? ex_rs                                                   : id_rs);
+        ex_rt                   <= (rst) ? 5'b0  : ((ex_stall & ~id_flush) ? ex_rt                                                   : id_rt);
+        ex_dp_hazard            <= (rst) ? 4'b0  : ((ex_stall & ~id_flush) ? ex_dp_hazard            : ((id_stall | id_flush) ? 4'b0 : id_dp_hazard));
+        ex_sign_imm16           <= (rst) ? 17'b0 : ((ex_stall & ~id_flush) ? ex_sign_imm16                                           : id_imm_extended);
+        ex_cp0_data             <= (rst) ? 32'b0 : ((ex_stall & ~id_flush) ? ex_cp0_data                                             : id_cp0_data);
+        ex_exception_pc         <= (rst) ? 32'b0 : ((ex_stall & ~id_flush) ? ex_exception_pc                                         : id_exception_pc);
+        ex_movn                 <= (rst) ? 1'b0  : ((ex_stall & ~id_flush) ? ex_movn                 : ((id_stall | id_flush) ? 1'b0 : id_movn));
+        ex_movz                 <= (rst) ? 1'b0  : ((ex_stall & ~id_flush) ? ex_movz                 : ((id_stall | id_flush) ? 1'b0 : id_movz));
+        ex_llsc                 <= (rst) ? 1'b0  : ((ex_stall & ~id_flush) ? ex_llsc                                                 : id_llsc);
+        ex_kernel_mode          <= (rst) ? 1'b0  : ((ex_stall & ~id_flush) ? ex_kernel_mode                                          : id_kernel_mode);
+        ex_is_bds               <= (rst) ? 1'b0  : ((ex_stall & ~id_flush) ? ex_is_bds                                               : id_is_bds);
+        ex_trap                 <= (rst) ? 1'b0  : ((ex_stall & ~id_flush) ? ex_trap                 : ((id_stall | id_flush) ? 1'b0 : id_trap));
+        ex_trap_condition       <= (rst) ? 1'b0  : ((ex_stall & ~id_flush) ? ex_trap_condition                                       : id_trap_condition);
+        ex_ex_exception_source  <= (rst) ? 1'b0  : ((ex_stall & ~id_flush) ? ex_ex_exception_source  : ((id_stall | id_flush) ? 1'b0 : id_ex_exception_source));
+        ex_mem_exception_source <= (rst) ? 1'b0  : ((ex_stall & ~id_flush) ? ex_mem_exception_source : ((id_stall | id_flush) ? 1'b0 : id_mem_exception_source));
     end // always @ (posedge clk)
 endmodule // antares_idex_register
